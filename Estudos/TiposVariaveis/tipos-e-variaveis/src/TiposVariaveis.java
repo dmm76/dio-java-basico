350a2eb0424829c69837df82d014712b12428a61
dmm76 <douglas.monquero@gmail.com>
2024-02-18 17:06:25 -0300

    tipos de dados e casting

--- Estudos/TiposVariaveis/tipos-e-variaveis/src/TiposVariaveis.java
+++ Estudos/TiposVariaveis/tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        byte idade = 33;
+        short ano = 2021;
+        int cep = 87060110;
+        long cpf = 987654321;
+        float pi = 3.14f;
+        double salario = 5123.56;
+
+        short numeroCurto = 100;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //erro para demonstração // casting (short) 
+
+        // short numeroCurto = 100;
+        // short numeroNormal2 = 1000;
+        // int numeroNormal = numeroCurto;
+        // short numeroCurto2 = numeroNormal2;
+
+        System.out.println(numeroCurto2);
+
     }
 }
