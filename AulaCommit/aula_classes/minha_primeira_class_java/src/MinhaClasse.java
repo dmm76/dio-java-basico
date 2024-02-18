0fdd4d64c5fc1164aebd528b331fd19d42168a28
dmm76 <douglas.monquero@gmail.com>
2024-02-18 14:18:32 -0300

    andamento01

--- AulaCommit/aula_classes/minha_primeira_class_java/src/MinhaClasse.java
+++ AulaCommit/aula_classes/minha_primeira_class_java/src/MinhaClasse.java
@@ -1,5 +1,7 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Ola Mundo, sejam bem vindos");
+        //System.out.println("Ola Mundo, sejam bem vindos");
+        final String Br = "Brasil";
+        System.out.println(Br);
     }
 }
