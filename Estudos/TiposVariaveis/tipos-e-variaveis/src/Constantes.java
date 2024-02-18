d06265506d9e3ce504d612df2bfd731fc3786ee6
dmm76 <douglas.monquero@gmail.com>
2024-02-18 17:17:24 -0300

    constante

---
+++ Estudos/TiposVariaveis/tipos-e-variaveis/src/Constantes.java
@@ -0,0 +1,13 @@
+public class Constantes {
+    public static void main(String[] args) {
+        
+        int numero = 10;
+        numero = 125;
+
+        final double PI = 3.14;
+
+        System.out.println(PI);
+        System.out.println(numero);
+
+    }
+}
