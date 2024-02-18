055c15168d4932fc0017d39f60e59936696d9922
dmm76 <douglas.monquero@gmail.com>
2024-02-18 14:09:24 -0300

    aula02

--- AulaCommit/aula_classes/minha_primeira_class_java/src/MinhaClasse.java
+++ AulaCommit/aula_classes/minha_primeira_class_java/src/MinhaClasse.java
@@ -1,5 +1,5 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Ola Mundo");
+        System.out.println("Ola Mundo, sejam bem vindos");
     }
 }
