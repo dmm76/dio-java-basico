ac3610a77e98cb544bba257a3b84c1db44a5e547
dmm76 <douglas.monquero@gmail.com>
2024-02-19 15:14:40 -0300

    operadores

---
+++ Estudos/TiposVariaveis/tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,12 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+        a = 5;
+        b = 6;
+
+        //String resultado = a==b ? "verdadeiro" : "falso";
+        int resultado = a==b ? 1 : 2;
+        System.out.println(resultado);
+
+    }
+}
