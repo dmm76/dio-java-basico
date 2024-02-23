5cc756eed45fd6eaa7d9652eda3d9f2ac3f659c4
dmm76 <douglas.monquero@gmail.com>
2024-02-23 18:21:00 -0300

    fluxosimples

---
+++ Controle de Fluxo/Condicional Simples/controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.xml.sax.SAXNotRecognizedException;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        double saldo = 25.0;
+        double valorsolicitado = 0.0;
+        System.out.println("Digite o valor solicitado para retirada: ");
+        valorsolicitado = scanner.nextDouble();
+
+        if (valorsolicitado<saldo) {
+            saldo = saldo - valorsolicitado;
+        }
+        System.out.println("Seu saldo é de: R$ "+ saldo + " reais.");
+    }
+}
