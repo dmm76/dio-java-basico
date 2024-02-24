178f7e221b37645dc1b9dc5b1a8d4bc2d33a02fb
dmm76 <douglas.monquero@gmail.com>
2024-02-24 16:30:23 -0300

    do-while

--- Controle de Fluxo/SwitchCase/switch-case/src/PlanoOperadora.java
+++ Controle de Fluxo/SwitchCase/switch-case/src/PlanoOperadora.java
@@ -5,24 +5,35 @@ public class PlanoOperadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Escolha entre os Plano: 1: Basic| 2: Midia| 3: Turbo");
-        System.out.println("Escolha: 1 para Plano Basic - 100 minutos de ligação");
-        System.out.println("Escolha: 2 para Plano Midia - 100 minutos de ligação + Whatsapp + Instagram gratis");
-        System.out.println("Escolha: 3 para Plano Turbo - 100 minutos de ligação + Whatsapp + Instagram + 5Gb Youtube gratis");
-        String plano = scanner.nextLine();
+        int opt = 0;
+    do {
+        System.out.println("Escolha entre os Plano: 1: Basic| 2: Midia| 3: Turbo.");
+        System.out.println("Escolha: 1 para Plano Basic - 100 minutos de ligação.");
+        System.out.println("Escolha: 2 para Plano Midia - 100 minutos de ligação + Whatsapp + Instagram gratis.");
+        System.out.println("Escolha: 3 para Plano Turbo - 100 minutos de ligação + Whatsapp + Instagram + 5Gb Youtube gratis.");
+        System.out.println("Escolha: 4 para Sair do Menu.");
+        opt = scanner.nextInt();
         
-        switch (plano) {
-            case "1":
-            System.out.println("Plano Basic escolhido\nAgradecemos por escolher nossa empresa");
+        switch (opt) {
+            case 1:
+                System.out.println("Plano Basic escolhido\nAgradecemos por escolher nossa empresa");
                 break;
-            case "2":
+            case 2:
                 System.out.println("Plano Midia + Whatsapp e Instagram grátis\nAgradecemos por escolher nossa empresa");
                 break;
-            case "3":
+            case 3:
                 System.out.println("Plano Turbo + Whatsapp + Instagram e 5Gb Youtube grátis\nAgradecemos por escolher nossa empresa");
-                break;    
+                break;
+            case 4:
+                System.out.println("Saindo do Menu\nObrigado por usar nossos serviços.\n");
+                break;
             default:
+                System.out.println("Opção inválida - escolha um plano 4 para sair");
                 break;
         }
-    }
+        
+
+    }while (opt != 4);
+      
+    } 
 }
