c2ab9d5adadfbb0d0fa63661dbfaad957fe8b1ca
dmm76 <douglas.monquero@gmail.com>
2024-02-26 12:28:42 -0300

    foreach

---
+++ EstruturaRepeticao/comando-for/src/ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach {
+    public static void main(String[] args) {
+            String alunos[] = {"Douglas", "Marcelo", "Monquero", "Proximo"};
+
+            //forma abreviada
+            for (String aluno : alunos) {
+                System.out.println("O nome do aluno é: " + aluno);
+            }
+    }
+}
