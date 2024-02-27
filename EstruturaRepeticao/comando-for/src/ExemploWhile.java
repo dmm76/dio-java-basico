ceb92734b5e01e59c704638cacb303bf37b27eb6
dmm76 <douglas.monquero@gmail.com>
2024-02-26 21:56:17 -0300

    do while

--- EstruturaRepeticao/comando-for/src/ExemploWhile.java
+++ EstruturaRepeticao/comando-for/src/ExemploWhile.java
@@ -6,14 +6,12 @@ public class ExemploWhile {
         int cont = 0;
 
         while (mesada>0) {
-            
-            
-
             double valorDoce = ValorAleatorio();
             
-            if(valorDoce > mesada) // na ultima iteracao se o valor do doce for maior
-            //valorDoce = mesada;     // valor do doce sera igualado ao valor de mesada
-            break; //vai parar a iteracao aqui quando nao puder comprar o doce mais
+            if(valorDoce > mesada) 
+                //valorDoce = mesada;    // na ultima iteracao se o valor do doce for maior
+                 //valorDoce = mesada;     // valor do doce sera igualado ao valor de mesada
+                break; //vai parar a iteracao aqui quando nao puder comprar o doce mais
 
             System.out.println("Valor do doce "+ ++cont + ": " + valorDoce + " adicionado ao carrinho");
             mesada = mesada - valorDoce;
