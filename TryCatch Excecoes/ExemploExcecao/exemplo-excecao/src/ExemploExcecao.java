d3f15c27da8f303344c8a72f5d36d8ace8318eba
dmm76 <douglas.monquero@gmail.com>
2024-02-27 20:58:11 -0300

    erroruntime

---
+++ TryCatch Excecoes/ExemploExcecao/exemplo-excecao/src/ExemploExcecao.java
@@ -0,0 +1,18 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        Number valor;
+        //Number valor = Double.valueOf("a1.75"); //valueof convert string em double
+        //Erro Runtime Exception
+        try {
+            valor = NumberFormat.getInstance().parse("a1.75");
+            //Erro Exception
+            System.out.println(valor);
+            } catch (ParseException e) {
+                //e.printStackTrace();
+                System.out.println("Nao pode haver caracteres alfanumericos");
+            }
+    }
+}
